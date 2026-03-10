51eb8a9131bbeaed0a2b908af837dead205a22d2
keithsp <keithchua2002@gmail.com>
2026-03-11 03:33:11 +0800

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+# BROKER = "test.mosquitto.org"
+# BROKER = "127.0.0.1"
+BROKER = "192.168.137.1"
+
+PORT = 1883
+TOPIC = "Some topic"
+
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("Connected with result code", rc)
+    client.subscribe(TOPIC)
+
+def on_message(client, userdata, msg):
+    print("Received:", msg.payload.decode())
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER, PORT, 60)
+client.loop_forever()
+
+
